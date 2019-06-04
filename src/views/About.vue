badf1037b8fa0b0b681c7a350a5ff2c4e1b5a3e1
teo <ldptek@gmail.com>
2019-06-04 15:33:08 +0100

    Change About page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,9 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1> About</h1>
+    <p> A simple TodoList app.</p>
+    <p> Created following a Vue Crash Course</p>
+    <iframe width="800px" height="500px"
+      src="https://www.youtube.com/embed/Wy9q22isx3U" frameborder="0" allowfullscreen></iframe> 
   </div>
 </template>
