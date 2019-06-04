a21114a31bc6f041eae97f335f4f23eae9ad841a
teo <ldptek@gmail.com>
2019-06-04 15:07:04 +0100

    AddTodo adding Todos

--- src/components/AddTodo.vue
+++ src/components/AddTodo.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
-        <form >
-            <input type="text" name="title" placeholder="Add Todo...">
+        <form @submit="addTodo">
+            <input type="text" v-model="title" name="title" placeholder="Add Todo...">
             <input type="submit" value="Submit" class="btn">
 
         </form>
@@ -9,8 +9,27 @@
 </template>
 
 <script>
+import uuid from "uuid";
 export default {
-    name:"AddTodo"
+    name:"AddTodo",
+    data() {
+        return {
+            title: ''
+        }
+    },
+    methods: {
+        addTodo(e) {
+            e.preventDefault();
+            const newTodo = {
+                id: uuid.v4(),
+                title: this.title,
+                completed: false
+            }
+            //send to parent
+            this.$emit('add-todo', newTodo);
+            this.title = '';
+        }
+    }
 }
 </script>
 
@@ -28,7 +47,7 @@ export default {
         flex:2;
     }
 
-    
+
 </style>
 
 
